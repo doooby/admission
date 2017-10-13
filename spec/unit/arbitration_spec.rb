2859f286783c04bdcdcdc6f2058f550f6fa809f4
zelazko <zelazk.o@email.cz>
2017-10-13 22:49:02 +0200

    added test to rules builder (restrains from using internal action name :all)

---
+++ spec/unit/arbitration_spec.rb
@@ -0,0 +1,33 @@
+require_relative './_helper'
+
+RSpec.describe Admission::Arbitration do
+
+  describe 'RulesBuilder' do
+
+    describe 'fails when given reserved action name' do
+
+      let(:builder){
+        builder = Admission::Arbitration::RulesBuilder.new nil
+        builder.instance_variable_set '@privilege', 'privilege'
+        builder
+      }
+
+      it '#allow' do
+        expect{ builder.allow '', :allow }.not_to raise_exception
+        expect{ builder.allow '', Admission::ALL_ACTION }.to(
+            raise_exception("reserved action name #{Admission::ALL_ACTION}")
+        )
+      end
+
+      it '#forbid' do
+        expect{ builder.forbid '', :forbid }.not_to raise_exception
+        expect{ builder.forbid '', Admission::ALL_ACTION }.to(
+            raise_exception("reserved action name #{Admission::ALL_ACTION}")
+        )
+      end
+
+    end
+
+  end
+
+end
--- spec/unit/resource_arbitration_spec.rb
+++ spec/unit/resource_arbitration_spec.rb
@@ -38,4 +38,39 @@ RSpec.describe Admission::ResourceArbitration do
 
   end
 
+  describe 'RulesBuilder' do
+
+    describe 'fails when given reserved action name' do
+
+      let(:builder){
+        builder = Admission::ResourceArbitration::RulesBuilder.new nil
+        builder.instance_variable_set '@privilege', 'privilege'
+        builder
+      }
+
+      it '#allow' do
+        expect{ builder.allow '', :allow }.not_to raise_exception
+        expect{ builder.allow '', Admission::ALL_ACTION }.to(
+            raise_exception("reserved action name #{Admission::ALL_ACTION}")
+        )
+      end
+
+      it '#forbid' do
+        expect{ builder.forbid '', :forbid }.not_to raise_exception
+        expect{ builder.forbid '', Admission::ALL_ACTION }.to(
+            raise_exception("reserved action name #{Admission::ALL_ACTION}")
+        )
+      end
+
+      it '#allow_resource' do
+        expect{ builder.allow_resource :'', :allow_resource, &->{} }.not_to raise_exception
+        expect{ builder.allow_resource :'', Admission::ALL_ACTION, &->{} }.to(
+            raise_exception("reserved action name #{Admission::ALL_ACTION}")
+        )
+      end
+
+    end
+
+  end
+
 end
